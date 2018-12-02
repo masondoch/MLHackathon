9f5c78c2f810e6100d80efccbbdf57149f41f645
Mason Dochterman <masondoch@berkeley.edu>
2018-12-02 11:06:46 -0800

    removing punctuation

--- conversation.py
+++ conversation.py
@@ -1,5 +1,8 @@
 import numpy as np
 import sklearn
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
 class Conversation:
 
@@ -18,4 +21,8 @@ class Conversation:
         some input from the user, and return the output
         from the chatbot.
         '''
+        punct_array = [',', '.', '?', '!', '-', ';', ':']
+        word_array = nltk.word_tokenize(sentence)
+        filtered_word_array = [word for word in word_array if word not in punct_array]
+
         return sentence
