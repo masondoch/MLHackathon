9dcc264c9de412209a023163454312898049c0d6
Mason Dochterman <masondoch@berkeley.edu>
2018-12-02 11:50:27 -0800

    messages

--- conversation.py
+++ conversation.py
@@ -28,21 +28,21 @@ class Conversation:
         syn_last_words = []
         syn_delete_words = []
         syn_number_words= []
-        for i in start_words: 
+        for i in start_words:
             syns = wn.synsets(i)
-            for j in syns: 
+            for j in syns:
                 syn_start_words.append(syns[j].lemmas()[0].name())
-        for i in last_words: 
+        for i in last_words:
             syns = wn.synsets(i)
-            for j in syns: 
+            for j in syns:
                 syn_last_words.append(syns[j].lemmas()[0].name())
-        for i in delete_words: 
+        for i in delete_words:
             syns = wn.synsets(i)
-            for j in syns: 
+            for j in syns:
                 syn_delete_words.append(syns[j].lemmas()[0].name())
-        for i in number_words: 
+        for i in number_words:
             syns = wn.synsets(i)
-            for j in syns: 
+            for j in syns:
                 syn_number_words.append(syns[j].lemmas()[0].name())
         self.start_words = syn_start_words
         self.last_words = syn_last_words
@@ -68,7 +68,20 @@ class Conversation:
 
         lem_sentence = [wnl.lemmatize(i) for i in filtered_sentence]
 
+        start_response = "OK, what would you like me to say?"
+        last_response = "Your last note was: "
+        delete_response = "OK, I deleted your previous note."
+        number_response = "You have x notes."
+
+        for word in lem_sentence:
+            if word in self.start_words:
+                return start_response;
+            if word in self.last_words:
+                return last_response
+            if word in self.delete_words:
+                return delete_response
+            if word in self.number_words:
+                return number_response
 
 
-
-        return sentence
+        return "Sorry, I didn't understand that."
